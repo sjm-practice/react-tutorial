3744f8b97598c6ba6a0e4c4f2d75c88d222d7ceb
stevenjmarsh <sjm.atl@gmail.com>
2016-10-18 16:54:33 -0400

    adding comment describing React backdoor api to set innerHTML

--- public/scripts/components/Comment.js
+++ public/scripts/components/Comment.js
@@ -5,6 +5,13 @@ const commentRawMarkup = (markupText) => {
   return { __html: rawMarkup };
 };
 
+/**
+ * This uses a special API that intentionally makes it difficult to insert raw HTML,
+ * but for remarkable we'll take advantage of this backdoor.
+ *
+ * Remember: by using this feature you're relying on remarkable to be secure. In this case,
+ * remarkable automatically strips HTML markup and insecure links from the output.
+ */
 const Comment = (props) => {
   return (
     <div className="comment">
