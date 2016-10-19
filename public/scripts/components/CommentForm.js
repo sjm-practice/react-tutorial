2aa03be512f20dbe633ef2383503a4d1919287fc
stevenjmarsh <sjm.atl@gmail.com>
2016-10-19 11:43:10 -0400

    forgot to add rendering of CommentFormContainer, also adding proptypes for CommentForm

--- public/scripts/components/CommentForm.js
+++ public/scripts/components/CommentForm.js
@@ -4,16 +4,23 @@ const CommentForm = (props) => {
       <input
         type="text"
         placeholder="Your Name"
-        onChange={props.handleAuthorChange}
+        onChange={props.onAuthorChange}
         value={props.author}
       />
       <input
         type="text"
         placeholder="say something..."
-        onChange={props.handleTextChange}
+        onChange={props.onTextChange}
         value={props.text}
       />
       <input type="submit" value="post" />
     </form>
   );
+};
+
+CommentForm.propTypes = {
+  onAuthorChange: React.PropTypes.func.isRequired,
+  onTextChange: React.PropTypes.func.isRequired,
+  author: React.PropTypes.string.isRequired,
+  text: React.PropTypes.string.isRequired,
 };
